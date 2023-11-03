1d0537bc994119aba52ee325d8d7f4e2b281f1ac
david-vargas98 <edgar981125@live.com.mx>
2023-11-03 17:31:46 -0600

    Se agrega un segmento para detctar errores y mostrarlos

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -11,6 +11,18 @@
         </div>
     </div>
 
+    {{-- Se muestran los errores al crear registros --}}
+    @if ($errors->any())
+    <div class="alert alert-danger mt-2">
+        <strong>Por las chancas de mi madre!</strong> Algo fue mal..<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+
     <form action="{{route('tasks.store')}}" method="POST">
         @csrf {{-- Se utiliza para evitar ataques cross-site request forgery --}}
         <div class="row">
